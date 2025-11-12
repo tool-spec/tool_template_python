5b56665567d29d11834fe7622fd392b2e1a07ef5
Mirko Mälicke <mirko@hydrocode.de>
2025-11-12 11:19:07 +0100

    Add docker compose and use logger in template

--- src/run.py
+++ src/run.py
@@ -1,9 +1,9 @@
 import os
 from datetime import datetime as dt
-from pprint import pprint
 
 from json2args import get_parameter
 from json2args.data import get_data
+from json2args.logger import logger
 
 # parse parameters
 kwargs = get_parameter()
@@ -15,14 +15,14 @@ toolname = os.environ.get('TOOL_RUN', 'foobar').lower()
 # switch the tool
 if toolname == 'foobar':
     # RUN the tool here and create the output in /out
-    print('This toolbox does not include any tool. Did you run the template?\n')
+    logger.info('This toolbox does not include any tool. Did you run the template?\n')
     
     # write parameters to STDOUT.log
-    pprint(kwargs)
+    logger.info(kwargs)
 
     for name, ds in data.items():
-        print(f"\n### {name}")
-        print(ds)
+        logger.info(f"\n### {name}")
+        logger.info(ds)
     
 
 # In any other case, it was not clear which tool to run
